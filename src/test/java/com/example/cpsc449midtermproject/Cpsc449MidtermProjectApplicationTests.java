da0119120bd259de022299a0418d58f9a575a387
AzaanMP <azaanmp2@csu.fullerton.edu>
2026-03-13 18:38:46 -0700

    Spring Boot Initial Commit

---
+++ src/test/java/com/example/cpsc449midtermproject/Cpsc449MidtermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cpsc449midtermproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cpsc449MidtermProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
