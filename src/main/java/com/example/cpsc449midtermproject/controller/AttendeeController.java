6a12e333134cd182ac276b7ac3e4d94170b642af
AlejGalvan <alexigalvan04@gmail.com>
2026-03-15 21:50:06 -0700

    Implemented AttendeeController Updated AttendeeService and AttendeeBookingsDTO to support AttendeeController

--- src/main/java/com/example/cpsc449midtermproject/controller/AttendeeController.java
+++ src/main/java/com/example/cpsc449midtermproject/controller/AttendeeController.java
@@ -1,4 +1,38 @@
 package com.example.cpsc449midtermproject.controller;
 
+import com.example.cpsc449midtermproject.dto.AttendeeBookingsDTO;
+import com.example.cpsc449midtermproject.entity.Attendee;
+import com.example.cpsc449midtermproject.service.AttendeeService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/attendees")
 public class AttendeeController {
+
+    private final AttendeeService attendeeService;
+
+    public AttendeeController(AttendeeService attendeeService) {
+        this.attendeeService = attendeeService;
+    }
+
+    // Register a new attendee
+    @PostMapping
+    public ResponseEntity<Attendee> createAttendee(@RequestBody Attendee attendee) {
+
+        Attendee savedAttendee = attendeeService.createAttendee(attendee);
+
+        return new ResponseEntity<>(savedAttendee, HttpStatus.CREATED);
+    }
+
+    // Get all bookings for an attendee
+    @GetMapping("/{id}/bookings")
+    public ResponseEntity<AttendeeBookingsDTO> getAttendeeBookings(@PathVariable Long id) {
+
+        AttendeeBookingsDTO attendeeBookings = attendeeService.getBookingsForAttendee(id);
+
+        return ResponseEntity.ok(attendeeBookings);
+    }
 }
--- src/main/java/com/example/cpsc449midtermproject/dto/AttendeeBookingsDTO.java
+++ src/main/java/com/example/cpsc449midtermproject/dto/AttendeeBookingsDTO.java
@@ -2,8 +2,12 @@ package com.example.cpsc449midtermproject.dto;
 
 import lombok.Data;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AttendeeBookingsDTO {
     private String attendeeName;
     private List<BookingResponseDTO> bookings;
